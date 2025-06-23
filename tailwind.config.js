2f0e6da1e603504ab0ed32c92a98330ebb7c24c4
AntonRudko <anton_rudko@knu.ua>
2025-06-23 20:25:27 +0300

    first section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.html', './src/**/*.js'],
+	content: [],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dark-gray': '#363636',
+			},
+		},
 	},
 	plugins: [],
 }
